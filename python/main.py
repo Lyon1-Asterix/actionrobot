2b9a6398858e721c51fadb0c33739b58743dd7e0
gus3000 <gus3000spam@gmail.com>
2018-05-23 14:01:42 +0200

    ajout squelette primitif de la classe Robot

---
+++ python/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import ev3dev
+import ev3dev.ev3
+
+
+class Robot:
+    def __init__():
+        pass
+    
+    def avancer(nb_cases=1):
+        pass
+    
+    def reculer(nb_cases=1):
+        pass
+    
+    #sens trigo ?
+    def tourner(rotation=1):
+        pass
+    
+    def victoire():
+        pass
